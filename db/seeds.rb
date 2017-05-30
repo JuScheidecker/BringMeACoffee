bc1ee8f4eacdabbd7f7b52c2c4e04a44a2152de7
Juliette Scheidecker <juscheidecker@gmail.com>
2017-05-30 10:33:29 +0200

    intial seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,150 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#------.destroy_all----#
+OrderItems.destroy_all
+ChildOrders.destroy_all
+Order.destroy_all
+Item.destroy_all
+Shop.destroy_all
+User.destroy_all
+
+#------SEED de USERS-----#
+vincent =  User.create!(
+  first_name:'Vincent',
+  last_name: 'Textoris',
+  email:'vincent.textoris@gmail.com',
+  phone: '0611111111',
+  delivery_address: '2 avenue des saules, lille',
+  lat: 50.632811,
+  lng: 3.017600,
+  password: '123456'
+  )
+
+romain =  User.create!(
+  first_name:'Romain',
+  last_name: ' Despature',
+  email:'rdespature @gmail.com',
+  phone: '0622222222',
+  delivery_address: '165 Avenue de Bretagne, lille',
+  lat: 50.633247,
+  lng: 3.020348,
+  password: '123456'
+  )
+
+manuel =  User.create!(
+  first_name:'Manuel',
+  last_name: 'Sabine',
+  email:'manuel.sabine@gmail.com',
+  phone: '0633333333',
+  delivery_address: '2 Allée de la haye du temple, lomme',
+  lat: 50.632736,
+  lng: 3.018513,
+  password: '123456'
+  )
+
+juliette =  User.create!(
+  first_name:'Juliette',
+  last_name: 'Scheidecker ',
+  email:'juscheidecker@gmail.com',
+  phone: '0644444444',
+  delivery_address: '124 Avenue de Bretagne, lille',
+  lat: 50.634871,
+  lng: 3.021997,
+  password: '123456'
+  )
+
+#------SEED de SHOPS-----#
+sogood = Shop.create!(
+  name:'So Good',
+  delivery_address: '163 Avenue de Bretagne, Lille',
+  lat: 50.634384,
+  lng: 3.021068
+  )
+
+classcroute = Shop.create!(
+  name:'Class croute',
+  delivery_address: '166 Avenue de Bretagne, Lille',
+  lat: 50.634422,
+  lng: 3.020132
+  )
+
+#------SEED d' ITEM-----#
+latte_sogood = Item.create!(
+  name:'Cafe latte',
+  description:'Un mélange de 50% robusta et 50% arabica',
+  price: 3.5 ,
+  shop: sogood,
+  )
+
+capuccino_sogood = Item.create!(
+  name:'Capuccino',
+  description: "Café italien, servi dans une grande tasse, à base d'un expresso et coiffé de sa mousse de lait",
+  price: 4.2 ,
+  shop: sogood,
+  )
+
+frappucino_sogood = Item.create!(
+  name:'Frappucino',
+  description: "Summer capuccino",
+  price: 4.2 ,
+  shop: sogood,
+  )
+
+expresso_classcroute = Item.create!(
+  name:'Expresso',
+  description: "Café court, très corsé avec un arôme puissant",
+  price: 2.5 ,
+  shop: classcroute,
+  )
+
+theglace_classcroute = Item.create!(
+  name:'The glacé',
+  description: "Du thé vert, des glaçons et un soupçon de citron",
+  price: 3.5 ,
+  shop: classcroute,
+  )
+
+infusion_classcroute = Item.create!(
+  name:'Infusion',
+  description: "Petite tisane pour les amateurs de sensations douces",
+  price: 3 ,
+  shop: classcroute,
+  )
+
+#------SEED d' ORDER-----#
+order1_pending_takeaway = Order.create!(
+  user: vincent,
+  status: false, #pending
+  delivery_type: false #take away
+  )
+
+order2_pending_delivery = Order.create!(
+  user: romain,
+  status: false, #pending
+  delivery_type: true # delivery
+  )
+
+order3_confirmed_takeaway = Order.create!(
+  user: manuel,
+  status: true, #confirmed
+  delivery_type: false #take away
+  )
+
+order4_confirmed_delivery = Order.create!(
+  user: juliette,
+  status: true, #confirmed
+  delivery_type: true # delivery
+  )
+
+#------SEED de CHILD ORDER-----#
+ChildOrder.create!(main_order: order3_confirmed_takeaway, order: order2_pending_delivery)
+# ChildOrder.create!(main_order: order3_confirmed_takeaway, order: order4_confirmed_delivery)
+
+#------SEED d' ORDER ITEM-----#
+OrderItem.create!(order: order1_pending_takeaway, item: latte_sogood)
+OrderItem.create!(order: order1_pending_takeaway, item: capuccino_sogood)
+OrderItem.create!(order: order1_confirmed_delivery, item: frappucino_sogood)
+OrderItem.create!(order: order2_pending_delivery, item: expresso_classcroute)
+OrderItem.create!(order: order2_pending_delivery, item: theglace_classcroute)
+OrderItem.create!(order: order2_pending_delivery, item: infusion_classcroute)
+OrderItem.create!(order: order3_confirmed_takeaway, item: theglace_classcroute)
+OrderItem.create!(order: order4_confirmed_delivery, item: latte_sogood)
+OrderItem.create!(order: order4_confirmed_delivery, item: frappucino_sogood)
