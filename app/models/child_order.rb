ea2638153b16a3d0bb46dd52df946edc29043c05
Vincent Textoris <vincent.textoris@gmail.com>
2017-05-30 11:28:38 +0200

    update models childorders to be tested

--- app/models/child_order.rb
+++ app/models/child_order.rb
@@ -1,3 +1,4 @@
 class ChildOrder < ApplicationRecord
-  belongs_to :orders
+  belongs_to :order
+  belongs_to :main_order
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :users
   has_many :order_items
-  has_many :child_orders, class_name: 'Order', foreign_key: 'main_order_id'
-  belongs_to :main_order, class_name: 'Order'
+
+  has_many :child_orders
+  has_many :orders, through: :child_orders, foreign_key: "main_order_id"
+  belongs_to :main_order, through: :child_orders
 end
+
+
