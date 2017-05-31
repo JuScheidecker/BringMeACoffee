2bee2bc7f067a162276e112baed8ecb43d2dcb5e
Vincent Textoris <vincent.textoris@gmail.com>
2017-05-31 15:43:34 +0200

    set empty cart from shop & changes routes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,14 +4,30 @@ class OrdersController < ApplicationController
   end
 
   def show
-    @Order = Order.find(params[:id])
+    @order = Order.find(params[:id])
+  end
+
+  def add_to_cart
+    session[:carts] = {} unless session[:carts].present?
+    session[:carts][params[:id]] = {} unless session[:carts][params[:id]].present?
+    session[:carts][params[:id]][params[:item_id]] = session[:carts][params[:id]][params[:item_id]].to_i + 1
+
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_path) }
+      format.json { render json: session[:json] }
+    end
   end
 
   def cart
-    @Order = Order.new
+    session[:carts]
+
+    respond_to do |format|
+      format.html { render 'orders/cart'}
+    end
   end
 
   def validate
+
   end
 
 end
