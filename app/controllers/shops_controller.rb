2d99e16512662509b21847e7b5d17584d5aea2fa
Vincent Textoris <vincent.textoris@gmail.com>
2017-05-31 15:43:34 +0200

    set empty cart from shop & changes routes

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,10 +1,19 @@
 class ShopsController < ApplicationController
+
   def index
     @shops = Shop.all
   end
 
   def show
     @shop = Shop.find(params[:id])
+    reset_cart
+  end
 
+  private
+
+  def reset_cart
+    session[:carts].each do |id, _|
+      session[:carts][id] = {} unless id == params[:id]
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,21 @@ Rails.application.routes.draw do
 
 devise_for :users
 
-  resources :shops, only: [:index, :show]
+  resources :shops, only: [:index, :show] do
+    member do
+      get 'add_to_cart', to: 'orders#add_to_cart'
+    end
+    member do
+      get 'cart', to: 'orders#cart'
+    end
+  end
 
   resources :orders, only: [:index, :show] do
     resources :order_items, only: [:create, :destroy]
     post 'validate', to: 'orders#validate'
   end
 
-  get 'cart', to: 'orders#cart'
+
 
   root to: 'pages#home'
 end
