7e121eef66a63ecf7b0c69d115031c6ed2e6f2b3
manuel sabine <manuel.sabine@gmail.com>
2017-06-01 18:06:43 +0200

    Gmap4rails commit 1

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,12 +1,27 @@
 class ShopsController < ApplicationController
 
   def index
-    @shops = Shop.all
+    # shop geolocation set-up
+    @shops = Shop.where.not(latitude: nil, longitude: nil)
+
+    @hash = Gmaps4rails.build_markers(@shops) do |shop, marker|
+      marker.lat shop.latitude
+      marker.lng shop.longitude
+      marker.infowindow render_to_string(partial: "/shops/map_box", locals: { shop: shop })
+    end
   end
 
   def show
     @shop = Shop.find(params[:id])
-    reset_cart(params[:id])
+    reset_cart(params[:id]) unless session[:carts] == nil
+
+    # shop geolocation set-up
+    @shop_coordinates = { lat: @shop.latitude, lng: @shop.longitude }
+
+    @hash = Gmaps4rails.build_markers(@shop) do |shop, marker|
+      marker.lat shop.latitude
+      marker.lng shop.longitude
+    end
   end
 
   private
