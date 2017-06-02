d3a8bdde26518555d76070e595a8f9ed3cbc229d
manuel sabine <manuel.sabine@gmail.com>
2017-06-02 12:47:29 +0200

    show-order-updated

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,8 +7,11 @@ class OrdersController < ApplicationController
   def show
 
     @Order = Order.find(params[:id])
-    # A setting orderitem pour le show des child_orders
+    # Setting orderitem pour le show des child_orders
     @orderitem = @Order.orders.first.order_items if @Order.orders.first
+    # Setting shop_id pour récupérer l'id du shop à partir de l'order
+    @orders = Order.all
+    @shop_id = @orders.first.order_items.first.item.shop_id
 
   end
 
